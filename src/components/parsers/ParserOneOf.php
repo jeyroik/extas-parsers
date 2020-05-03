b328174b42eae55c10ef7e87349c0ac164922c87
jeyroik <jeyroik@gmail.com>
2020-05-03 18:31:37 +0300

    added parser OneOf

---
+++ src/components/parsers/ParserOneOf.php
@@ -0,0 +1,98 @@
+<?php
+namespace extas\components\parsers;
+
+use extas\components\Item;
+use extas\interfaces\parsers\IParseDispatcher;
+
+/**
+ * Class ParserOneOfReplace
+ *
+ * @package extas\components\parsers
+ * @author jeyroik@gmail.com
+ */
+class ParserOneOf extends Item implements IParseDispatcher
+{
+    public const FIELD__PATTERN = 'pattern';
+    public const FIELD__DELIMITER = 'delimiter';
+
+    /**
+     * @param mixed $value
+     * @return mixed|string|string[]
+     */
+    public function __invoke($value)
+    {
+        if ($this->hasPattern($value, $this->getPattern())) {
+            $variants = $this->buildVariants(
+                $this->getMatches($value, $this->getPattern()),
+                $this->getDelimiter()
+            );
+            $rand = mt_rand(0, count($variants)-1);
+            return $variants[$rand];
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param string $listOfVariants
+     * @param string $delimiter
+     * @return array
+     */
+    protected function buildVariants(string $listOfVariants, string $delimiter): array
+    {
+        $list = explode($delimiter, $listOfVariants);
+        foreach ($list as $i => $v) {
+            $list[$i] = trim($v);
+        }
+
+        return $list;
+    }
+
+    /**
+     * @param $value
+     * @param $pattern
+     * @return string
+     */
+    protected function getMatches($value, $pattern): string
+    {
+        preg_match($pattern, $value, $matches);
+
+        return $matches[1] ?? '';
+    }
+
+    /**
+     * @param $value
+     * @param $pattern
+     * @return bool
+     */
+    protected function hasPattern($value, $pattern): bool
+    {
+        preg_match($pattern, $value, $matches);
+
+        return !empty($matches);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getPattern(): string
+    {
+        return $this->config[static::FIELD__PATTERN] ?? '\@oneof\((.*)\)';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getDelimiter(): string
+    {
+        return $this->config[static::FIELD__DELIMITER] ?? ',';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSubjectForExtension(): string
+    {
+        return 'extas.parser.one.of.replace';
+    }
+}
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -7,6 +7,7 @@ use extas\components\conditions\ConditionRepository;
 use extas\components\parsers\Parser;
 use extas\components\parsers\ParserCurrentDateTime;
 use extas\components\parsers\ParserNullValue;
+use extas\components\parsers\ParserOneOf;
 use extas\components\parsers\ParserSample;
 use extas\components\parsers\ParserSimpleReplace;
 use extas\components\SystemContainer;
@@ -134,6 +135,26 @@ class ParserTest extends TestCase
         );
 
         $parser->setParameterValue(ParserSimpleReplace::FIELD__PARAM_NAME, 'unknown');
+        $this->assertEquals(
+            'Test @test.status',
+            $parser->parse('Test @test.status')
+        );
+    }
+
+    public function testOneOf()
+    {
+        $parser = new Parser([
+            Parser::FIELD__CLASS => ParserOneOf::class,
+            Parser::FIELD__PARAMETERS => []
+        ]);
+
+        $this->assertTrue(
+            in_array(
+                $parser->parse('test is @oneof("smile", "is ok", "\"working\"")'),
+                ['test is smile', 'test is ok', 'test is "working"']
+            )
+        );
+
         $this->assertEquals(
             'Test @test.status',
             $parser->parse('Test @test.status')
